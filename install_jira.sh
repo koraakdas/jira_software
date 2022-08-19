524dd5614949216ad189506cb571f04a0b518779
koraakdas <103141664+koraakdas@users.noreply.github.com>
2022-08-19 22:32:37 +0530

    Update install_jira.sh

--- install_jira.sh
+++ install_jira.sh
@@ -6,7 +6,7 @@ function install_jira() {
 #AWS CLI Environment Variables
 export AWS_DEFAULT_REGION=us-east-1;
 
-wget https://www.atlassian.com/software/jira/downloads/binary/atlassian-jira-software-8.22.6-x64.bin
+wget https://www.atlassian.com/software/jira/downloads/binary/atlassian-jira-software-8.22.6-x64.bin;
 sudo chmod a+x atlassian-jira-software-8.22.6-x64.bin;
 sudo bash ./atlassian-jira-software-8.22.6-x64.bin -q -varfile response.varfile;
 sudo tar -xzf mysql-connector-java-8.0.30.tar.gz;
@@ -19,10 +19,10 @@ sudo /etc/init.d/jira start;
 #AWS CLI Environment Variables
 export AWS_DEFAULT_REGION=us-east-1;
 
-password=$(aws secretsmanager get-secret-value --secret-id MysqldbCreds --query 'SecretString' --output text | jq .password | tr -d \") 
-database=$(aws rds describe-db-instances --db-instance-identifier mysqldb-server --query DBInstances[0] --output json | jq .DBName | tr -d \")
-user=$(aws rds describe-db-instances --db-instance-identifier mysqldb-server --query DBInstances[0] --output json | jq .MasterUsername | tr -d \")
-hostname=$(aws rds describe-db-instances --db-instance-identifier mysqldb-server --query DBInstances[0] --output json | jq .Endpoint.Address | tr -d \")
+password=$(aws secretsmanager get-secret-value --secret-id MysqldbCreds --query 'SecretString' --output text | jq .password | tr -d \"); 
+database=$(aws rds describe-db-instances --db-instance-identifier mysqldb-server --query DBInstances[0] --output json | jq .DBName | tr -d \");
+user=$(aws rds describe-db-instances --db-instance-identifier mysqldb-server --query DBInstances[0] --output json | jq .MasterUsername | tr -d \");
+hostname=$(aws rds describe-db-instances --db-instance-identifier mysqldb-server --query DBInstances[0] --output json | jq .Endpoint.Address | tr -d \");
     
     
 #Passing Values to jira database config file
